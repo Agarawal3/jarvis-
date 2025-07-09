61a9b288fedd8c9bf46b400b2b519f7a90edec52
Segun Ajibola <144719426+Agarawal3@users.noreply.github.com>
2025-07-09 23:41:30 +0530

    helo

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,8 @@ import axios from 'axios';
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-      const serverUrl="http://localhost:8000";
+      //const serverUrl="http://localhost:8000";
+      const serverUrl = import.meta.env.VITE_BACKEND_URL;
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
