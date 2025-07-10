758afc59c3cde5393c4d625212ae31f8c882a0c6
Segun Ajibola <144719426+Agarawal3@users.noreply.github.com>
2025-07-10 11:43:11 +0530

    heart

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
       "http://localhost:5173",
-      "https://jarvis-ywzt.vercel.app"
+      "https://jarvis-ywzt.vercel.app/signup"
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
